2bd264b5acee12cc529b794c7fe5a9f054fd4f0d
lil2kki <142544489+thaLILNIKKI@users.noreply.github.com>
2026-06-29 13:25:13 +0400

    Update .lua

--- .lua
+++ .lua
@@ -4,6 +4,10 @@ local function loadCustomAsset(url, filename)
 end
 local themes = game:GetService("ReplicatedStorage"):FindFirstChild("ChaseThemes", true)
 :FindFirstChild("2011x", true).RETRO
+themes.LastLifeChase.SoundId = loadCustomAsset(
+    "https://github.com/thaLILNIKKI/Classic-2011x-New-Soundtrack-Outcome-Memories/releases/download/assets"
+    .."/LastLifeChase.mp3", "cache/RETRO.LastLifeChase.mp3"
+)
 themes.NormalChase.SoundId = loadCustomAsset(
     "https://github.com/thaLILNIKKI/Classic-2011x-New-Soundtrack-Outcome-Memories/releases/download/assets"
     .."/NormalChase.mp3", "cache/RETRO.NormalChase.mp3"
@@ -12,3 +16,8 @@ themes.Rage.SoundId = loadCustomAsset(
     "https://github.com/thaLILNIKKI/Classic-2011x-New-Soundtrack-Outcome-Memories/releases/download/assets"
     .."/Rage.mp3", "cache/RETRO.Rage.mp3"
 )
+themes.TerrorRadius.SoundId = loadCustomAsset(
+    "https://github.com/thaLILNIKKI/Classic-2011x-New-Soundtrack-Outcome-Memories/releases/download/assets"
+    .."/TerrorRadius.mp3", "cache/RETRO.TerrorRadius.mp3"
+)
+
